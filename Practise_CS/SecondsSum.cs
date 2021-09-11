d05bf9a2b0395808019b76d1455b5da85957d5f1
Anatoly <anatolypanushkin@mail.ru>
2021-09-11 22:17:31 +0300

    Complete version of Seconds Sum!

---
+++ Practise_CS/SecondsSum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Practise_CS
+{
+    public class SecondsSum
+    {
+        public static void SecSum(int total)
+        {
+            //Переводим общее время в минуты и секунды
+            var ts = TimeSpan.FromSeconds(total);
+            if (ts.Seconds < 10)
+            {
+                Console.WriteLine("{0}:0{1}", ts.Minutes, ts.Seconds);
+            }
+            else
+            {
+                Console.WriteLine("{0}:{1}",ts.Minutes,ts.Seconds);
+            }
+        }
+    }
+}
