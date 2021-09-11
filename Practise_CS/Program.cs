e78782baeb44d6414990119958a83a82bbb66419
Anatoly <anatolypanushkin@mail.ru>
2021-09-11 22:20:48 +0300

    Main

---
+++ Practise_CS/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace Practise_CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                int time = Int32.Parse(Console.ReadLine());
+                total += time;
+            }
+            SecondsSum.SecSum(total);
+        }
+    }
+}
