62490eb264f61925bd1057a91f5f3637342c6e79
Anatoly <anatolypanushkin@mail.ru>
2021-09-11 22:47:57 +0300

    Add the Information about speed!

---
+++ Practise_CS/InformationAboutSpeed.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Practise_CS
+{
+    public class InformationAboutSpeed
+    {
+        public static void Speed(double speed)
+        {
+            if (speed<=10)
+            {
+                Console.WriteLine("Медленно");
+            }
+            else
+            {
+                Console.WriteLine("Быстро");
+            }
+        }
+    }
+}
--- Practise_CS/Program.cs
+++ Practise_CS/Program.cs
@@ -1,19 +1,37 @@
 ﻿using System;
+using System.Diagnostics;
 using Microsoft.VisualBasic.CompilerServices;
 
 namespace Practise_CS
 {
     class Program
-    {
+    { 
         static void Main(string[] args)
         {
-            int total = 0;
-            for (int i = 0; i < 3; i++)
+            Console.WriteLine("1 - Суммирование секунд, " +
+                              "2 - Информация о скорости");
+            switch (Console.ReadLine())
             {
-                int time = Int32.Parse(Console.ReadLine());
-                total += time;
+                //Задача: Суммирование секунд
+                case "1":
+                    int total = 0;
+                    for (int i = 0; i < 3; i++)
+                    {
+                        int time = Int32.Parse(Console.ReadLine());
+                        total += time;
+                    }
+                    SecondsSum.SecSum(total);   
+                    break;
+                
+                //задача: Информация о скорости
+                case "2":
+                    InformationAboutSpeed.Speed(double.Parse(Console.ReadLine()));  
+                    break;
             }
-            SecondsSum.SecSum(total);
+            
+           
+            
+            
         }
     }
 }
